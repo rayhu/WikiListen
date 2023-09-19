c16bd9e25ab9947fbac712d274977cabbf8012b6
Ray Hu <rayhu007@gmail.com>
2023-09-19 09:24:58 -0700

    add check pr workflow

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,19 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!(react-native|react-native-fs|@react-native|@react-native/js-polyfills)/)',
   ],
+  collectCoverage: true,
+  coverageReporters: ['text', 'lcov'],
+  collectCoverageFrom: [
+    '{services, pages}/**/*.{ts,tsx}',
+    '!**/node_modules/**',
+    '!**/scripts/**',
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 90,
+      lines: 80,
+    },
+  },
 };
